e0fcb5fc1e27abc87ec22e260f92e3f460974fd2
Rolf K <abstractstar@gmail.com>
2011-10-30 19:52:18 +0100

    Data file parsing

---
+++ datafiles.h
@@ -0,0 +1,13 @@
+/*
+ * Gullible's Travails - 2011 Rewrite!
+ *
+ * Copyright 2011 Rolf Klausen
+ */
+#ifndef DATAFILES_H
+#define DATAFILES_H
+
+#define MAIN_DATA_FILE "data/data.cfg"
+
+int parse_data_files();
+
+#endif
